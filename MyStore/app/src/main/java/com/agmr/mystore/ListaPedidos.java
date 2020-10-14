86447316943fda634c5114dc4fea9e4583f35907
Andrés <freddy.sapatanga.est@tecazuay.edu.ec>
2020-10-14 10:52:29 -0500

    Login arreglado

--- MyStore/app/src/main/java/com/agmr/mystore/ListaPedidos.java
+++ MyStore/app/src/main/java/com/agmr/mystore/ListaPedidos.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Lista_pedidos extends AppCompatActivity {
+public class ListaPedidos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
